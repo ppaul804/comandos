f0c33dcbe29585ed2730a00a4ccf6fe5b32e16e8
Pedro Paul <ppaul804@gmail.com>
2021-03-26 13:38:37 -0300

    Create smbLinux.sh

---
+++ smbLinux.sh
@@ -0,0 +1,5 @@
+sudo su # entra como super usuario
+apt install samba # Samba é um software gratuito de uma re-implementação do protocolo de rede SMB
+cd /etc/samba # local onde fica o smb.conf
+cp smb.conf smb.bak.conf # faz um backup do arquivo de configuração do samba
+
