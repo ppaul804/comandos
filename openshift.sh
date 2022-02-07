19f94a036624eab5b1515544f8aaccf2bedaec33
Pedro Paul <ppaul804@gmail.com>
2022-02-07 14:00:28 -0300

    Create openshift.sh

---
+++ openshift.sh
@@ -0,0 +1,9 @@
+oc login
+# Lista todos projetos
+oc projects
+# Seleciona projeto
+oc project <project-name>
+# Procura pod
+oc get pods | Select-String <pod-name>
+# Abre terminal do pod
+oc rsh <pod-name>
