d13b4f4d0ea63d938c1995d96da31174143faaf8
Pedro Paul <ppaul804@gmail.com>
2022-02-07 14:57:23 -0300

    Update openshift.sh

--- openshift.sh
+++ openshift.sh
@@ -7,3 +7,5 @@ oc project <project-name>
 oc get pods | Select-String <pod-name>
 # Abre terminal do pod
 oc rsh <pod-name>
+# Deixar rota externa
+oc label route zync-3scale-api-7ftxb "router=external"
