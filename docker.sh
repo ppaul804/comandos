e813ee3bb3c10d2b31d0a7f8c03ab6b8cc794744
Pedro Paul <ppaul804@gmail.com>
2022-05-20 13:37:42 -0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -12,5 +12,5 @@ docker rm {container}
 docker ps
 # executar terminal do container
 docker exec -it {container} /bin/sh
-# docker-compose, suba
+# docker-compose, crie e inicie containers, rodando de fundo
 docker-compose up -d
