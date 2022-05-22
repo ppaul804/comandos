f2713a9b9feb26aaebd3d62616e7c3b86dfccbeb
Pedro Paul <ppaul804@gmail.com>
2022-05-22 17:47:09 -0300

    Update chocoInstall.bat

--- scripts/chocoInstall.bat
+++ scripts/chocoInstall.bat
@@ -43,7 +43,7 @@ IF %ERRORLEVEL% NEQ 0 (
 
 :chocoInstall
     @REM ---Dev---
-    @REM choco install openjdk8 openjdk maven vscode jmeter -y
+    @REM choco install jdk8 oraclejdk maven vscode jmeter -y
     @REM choco install openshift-cli minishift -y
     @REM choco install nodejs-lts nvm yarn -y
     @REM choco install microsoft-teams forticlientvpn git git-fork docker-desktop robo3t postman notepadplusplus drawio virtualbox -y
