0f4ba289f7f5b8cbffeb47affc036adcc68cfcd6
Pedro Paul <ppaul804@gmail.com>
2022-06-15 15:33:29 -0300

    Update minishift.ps1

--- minishift.ps1
+++ minishift.ps1
@@ -1,3 +1,7 @@
+# com Hyper-V
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 minishift start
+
+# com VirtualBox
+bcdedit /set hypervisorlaunchtype off
 minishift start --vm-driver virtualbox
