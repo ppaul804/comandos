e8b7a68a930133492ceb2cdf099d4813747a04c0
Pedro Paul <ppaul804@gmail.com>
2022-11-29 14:18:56 -0300

    Update openshift.sh

--- openshift.sh
+++ openshift.sh
@@ -9,3 +9,7 @@ oc get pods | Select-String <pod-name>
 oc rsh <pod-name>
 # Deixar rota externa
 oc label route <3scale-route-name> "router=external"
+# Criar um novo app usando um repositorio privado pela branch develop
+oc new-app --name=myapp https://github.com/youruser/yourprivaterepo#develop --source-secret=yoursecret
+# Criar um novo app usando um repositorio privado pela branch develop com a imagem do java
+oc new-app --name=myapp openshift/java:latest~https://github.com/youruser/yourprivaterepo#develop --source-secret=yoursecret
