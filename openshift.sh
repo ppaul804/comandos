2c56610ad806acdb088f841e172782d77f729b68
ppaul804 <ppaul804@gmail.com>
2023-10-04 11:12:05 -0300

    comandos openshift

--- openshift.sh
+++ openshift.sh
@@ -15,6 +15,10 @@ oc new-app --name=myapp https://github.com/youruser/yourprivaterepo#develop --so
 oc new-app --name=myapp openshift/java:latest~https://github.com/youruser/yourprivaterepo#develop --source-secret=yoursecret
 # Listar todos os recursos do app criado
 oc get all -o name --selector app=myapp
+# Exportar os recursos do app criado
+oc export bc,dc,is,svc,route,secret,sa -l app=myapp -o yaml > myapp.yaml
+# Aplicar os recursos do app criado
+oc apply -f myapp.yaml
 # Apagar todos os recursos do app criado
 oc delete all --selector app=myapp
 # Acessa o pod do mongo com a porta 27017
---
+++ scripts/iniciar email.bat
@@ -0,0 +1,4 @@
+@echo off
+@REM pausa de 10 segundos
+ping 127.0.0.1 -n 11 > nul
+start outlookmail:
