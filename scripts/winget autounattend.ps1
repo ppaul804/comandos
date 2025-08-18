0894c18e08879fd773d767c73c9e8bbbba187815
Pedro Paul <ppaul804@gmail.com>
2025-08-17 23:11:52 -0300

    scripts unattend

---
+++ scripts/winget autounattend.ps1
@@ -0,0 +1,18 @@
+if ( [System.Environment]::OSVersion.Version.Build -lt 26100 ) {
+    'This script requires Windows 11 24H2 or later.' | Write-Warning;
+    return;
+}
+$timeout = [datetime]::Now.AddMinutes( 5 );
+$exe = "$env:LOCALAPPDATA\Microsoft\WindowsApps\winget.exe";
+
+while ( $true ) {
+    if ( $exe | Test-Path ) {
+        & $exe install Google.Chrome Mozilla.Firefox.pt-BR VideoLAN.VLC geeksoftwareGmbH.PDF24Creator 7zip.7zip QBitTorrent.QBitTorrent Git.Git JAMSoftware.TreeSize.Free EclipseAdoptium.Temurin.21.JDK KeePassXCTeam.KeePassXC --silent --accept-package-agreements --accept-source-agreements --source winget --scope machine;
+        return;
+    }
+    if ( [datetime]::Now -gt $timeout ) {
+        'File {0} does not exist.' -f $exe | Write-Warning;
+        return;
+    }
+    Start-Sleep -Seconds 1;
+}
--- scripts/winget install.ps1
+++ scripts/winget install.ps1
@@ -4,17 +4,7 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Write-Host "O script precisa ser executado como administrador. Execute o prompt de comando como administrador e execute o script novamente."
     exit
 }
-winget install CPUID.CPU-Z
-winget install CPUID.HWMonitor
-winget install CrystalDewWorld.CrystalDiskInfo
-winget install CrystalDewWorld.CrystalDiskMark
-winget install Google.Chrome
-winget install Mozilla.Firefox
-winget install VideoLAN.VLC
-winget install Adobe.Acrobat.Reader.64-bit
-winget install Microsoft.PowerToys
-winget install 7zip.7zip
-winget install QBitTorrent.QBitTorrent
+winget install CPUID.CPU-Z CPUID.HWMonitor CrystalDewWorld.CrystalDiskInfo CrystalDewWorld.CrystalDiskMark Google.Chrome Mozilla.Firefox VideoLAN.VLC geeksoftwareGmbH.PDF24Creator Microsoft.PowerToys 7zip.7zip QBitTorrent.QBitTorrent --accept-package-agreements --accept-source-agreements --source winget
 if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Chocolatey não está instalado. Instalando..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
