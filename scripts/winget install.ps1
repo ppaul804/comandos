13d8c8a2b9b5f1cfbbc68619a72469637c10a19b
Pedro Paul <ppaul804@gmail.com>
2025-11-20 22:25:11 -0300

    Update scripts/winget install.ps1

--- scripts/winget install.ps1
+++ scripts/winget install.ps1
@@ -4,4 +4,10 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Write-Host "O script precisa ser executado como administrador. Execute o prompt de comando como administrador e execute o script novamente."
     exit
 }
-winget install IDRIX.VeraCrypt CPUID.CPU-Z CPUID.HWMonitor CrystalDewWorld.CrystalDiskInfo CrystalDewWorld.CrystalDiskMark Google.Chrome Mozilla.Firefox VideoLAN.VLC geeksoftwareGmbH.PDF24Creator Microsoft.PowerToys 7zip.7zip QBitTorrent.QBitTorrent --accept-package-agreements --accept-source-agreements --source winget
+winget install CPUID.CPU-Z CPUID.HWMonitor CrystalDewWorld.CrystalDiskInfo CrystalDewWorld.CrystalDiskMark Google.Chrome Mozilla.Firefox VideoLAN.VLC geeksoftwareGmbH.PDF24Creator Microsoft.PowerToys 7zip.7zip QBitTorrent.QBitTorrent --accept-package-agreements --accept-source-agreements --source winget
+if (!(Get-Command choco -ErrorAction SilentlyContinue)) {
+    Write-Host "Chocolatey não está instalado. Instalando..."
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+}
+choco install -y veracrypt
+choco install -y keepassxc 
